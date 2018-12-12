cfbec337b256c510ad15c99507aa9cede9ca528b
root <root@DESKTOP-NSML37J.localdomain>
2018-12-11 21:09:11 -0500

    added more logic

--- driver.cpp
+++ driver.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <sql.c>
+#include <stdio.h>
+#include <sqlite3.h>
 
 using namespace std;
 
 int main(){
-  bool finished = false;
+  bool loggedin = false;
   string input;
   string username;
   string password;
 
-  cout << endl;
-  cout << "Welcome to the super secret email server." << endl;
+  cout << "\nWelcome to the super secret email server." << endl;
 
-  while(!finished){
+  while(!loggedin){
     cout << "What would you like to do? Enter the number of your selected option." << endl;
     cout << "1.) Log In" << endl;
     cout << "2.) Register" << endl;
@@ -25,8 +27,7 @@ int main(){
       bool auth = false;
       int count = 0;
       while(login && count < 3){
-        cout << endl;
-        cout << "Your username?" << endl;
+        cout << "\nYour username?" << endl;
         cin >> username;
 
         cout << "Password?" << endl;
@@ -39,27 +40,64 @@ int main(){
         }
         else{
           count++;
-          cout << endl;
-          cout << "Invalid log in." << endl;
-          cout << "You have used " << count << " of 3 attempts." << endl;
-          cout << endl;
+          cout << "\nInvalid log in." << endl;
+          cout << "You have used " << count << " of 3 attempts.\n" << endl;
         }
       }
     }
+
     else if(input == "2"){
+
+      cout << "Enter your new username" << endl;
+      cin >> username;
+      //username cannot have ;- etc
+      //username cannot already match another username in the table
+
+      cout << "Enter your new password" << endl;
+      cin >> password;
+      //password cannot have ;- etc
+      //hash username w/ password, use salsa20?
+       
+      //-> INSERT INTO Users(Username, Password)
+      
 
     }
     else if (input == "3") {
-      finished = true;
-      cout << endl;
-      cout << "Bye bye!" << endl;
-      cout << endl;
+      loggedin = true;
+      cout << "\nBye bye!\n" << endl;
     }
     else{
-      cout << endl;
-      cout << "Your input did not match any of the options." << endl;
-      cout << "Please try again." << endl;
-      cout << endl;
+      cout << "\nYour input did not match any of the options.\n" << endl;
+      cout << "Please try again.\n" << endl;
     }
+
   }
+
+  int input2;
+  while(loggedin){
+    cout << "What would you like to do? Enter the number of your selected option." << endl;
+    cout << "1.) Send a message" << endl;
+    cout << "2.) View messages" << endl;
+    cout << "3.) Log out" << endl;
+    cin >> input2;
+
+    if(input2 == 1){
+        
+    }
+
+    else if(input2 == 2){
+
+    }
+
+    else if(input2 == 3){
+      loggedin = false;
+    }
+
+    else{
+      cout << "\nYour input did not match any of the options.\n" << endl;
+      cout << "Please try again.\n" << endl;
+    }
+
+  }
+
 }
