e3670be7052c199415d982caf52db2fd511431a9
root <root@DESKTOP-NSML37J.localdomain>
2018-12-12 19:51:13 -0500

    idk man

--- db.sql
+++ db.sql
@@ -2,8 +2,6 @@ PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 CREATE TABLE users (id integer primary key autoincrement, name text, password text);
 
-INSERT INTO "users" VALUES(2,'matt','SuperSecure00');
-INSERT INTO "users" VALUES(3,'kyle','SuperSecure01');
 
 CREATE TABLE messages (sender text, receiver text, message text);
 
